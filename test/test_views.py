96c094658c58d1a28990a41cdd374200e2587614
Tomasz Rembisz <tomaszrembisz@gmail.com>
2019-03-10 11:46:00 +0000

    poprawa imienia

--- test/test_views.py
+++ test/test_views.py
@@ -14,5 +14,5 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEquals('{ "imie":"Magdalena", "msg":"Hello World!"}',
+        self.assertEquals('{ "imie":"Tomasz", "msg":"Hello World!"}',
                           rv.data)
